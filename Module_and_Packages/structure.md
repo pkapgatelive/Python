9a16067e7b3db3415de7e670f3a5c4a30ee9ae38
pkapgatelive <59923992+pkapgatelive@users.noreply.github.com>
2023-11-19 16:08:09 +1100

    packages and modules programes has been exected

---
+++ Module_and_Packages/structure.md
@@ -0,0 +1,10 @@
+data_science_package/
+|-- __init__.py
+|-- data/
+|   |-- __init__.py
+|   |-- loader.py
+|   |-- preprocessing.py
+|-- models/
+|   |-- __init__.py
+|   |-- classifier.py
+|-- main_script.py
---
+++ modules_and_pakages.py
@@ -0,0 +1,17 @@
+import os
+
+def test(a,b):
+    return a+b
+
+
+def test1(a,b):
+    return a*b
+
+def test3(a,b):
+    return a/b
+
+def test4():
+    print("This is my code written in module_and_packages.py")
+
+
+
